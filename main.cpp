2ce4d590881da1ab11809decbf4cebae1724c78d
Steven Farabaugh <stevefireball@gmail.com>
2013-05-29 00:26:23 -0800

    Built Menu
    Built very initial menu, the journey of a thousand miles begins with a
    single step

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream.h>
+
+int main()
+{int menu1=0;
+cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+while (menu1 != 5)
+{cout << "\n\n\n\n\n\n\n\n    Main Menu\n1:New\n2:Load\n3:Options\n4:About\n5:Exit\n";
+cin >> menu1;
+}
+}
